b9dc68ef803b45a60436b78ed1b8a11f8ccb949f
healthy-pod <ahmad.abedalqader@konghq.com>
2025-04-10 08:50:21 -0600

    listplanmodifier: return null for non-computed attr when config is null
    Fixes #4

--- internal/planmodifiers/listplanmodifier/supress_zero_null_modifier.go
+++ internal/planmodifiers/listplanmodifier/supress_zero_null_modifier.go
@@ -1,9 +1,10 @@
 package listplanmodifier
 
 import (
-    "context"
+	"context"
 
-    "github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var _ planmodifier.List = ListSupressZeroNullModifierPlanModifier{}
@@ -23,7 +24,7 @@ func (v ListSupressZeroNullModifierPlanModifier) MarkdownDescription(ctx context
 // Validate performs the plan modification.
 func (v ListSupressZeroNullModifierPlanModifier) PlanModifyList(ctx context.Context, req planmodifier.ListRequest, resp *planmodifier.ListResponse) {
     if len(resp.PlanValue.Elements()) == 0 && len(req.StateValue.Elements()) == 0 && req.ConfigValue.IsNull() {
-        resp.PlanValue = req.StateValue
+        resp.PlanValue = types.ListNull(req.ConfigValue.ElementType(ctx))
     }
     //resp.Diagnostics.AddAttributeError(
     //	req.Path,
