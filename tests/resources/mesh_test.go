be575e8a10aee442be6222ed0a3a46a4d3987ede
slonka <slonka@users.noreply.github.com>
2025-04-29 16:25:05 +0200

    fix(retry_on): try to reproduce the issue
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- tests/resources/mesh_test.go
+++ tests/resources/mesh_test.go
@@ -2,6 +2,9 @@ package tests
 
 import (
 	"github.com/Kong/shared-speakeasy/tfbuilder"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
@@ -42,7 +45,7 @@ func TestMesh(t *testing.T) {
 
 	t.Run("create a policy and modify fields on it", func(t *testing.T) {
 		builder := tfbuilder.NewBuilder(tfbuilder.KongMesh, "http", "localhost", port.Int())
-		mesh := tfbuilder.NewMeshBuilder("default", "terraform-provider-kong-mesh")
+		mesh := tfbuilder.NewMeshBuilder("m2", "m2")
 		mtp := tfbuilder.NewPolicyBuilder("mesh_traffic_permission", "allow_all", "allow-all", "MeshTrafficPermission").
 			WithMeshRef(builder.ResourceAddress("mesh", mesh.ResourceName) + ".name").
 			WithDependsOn(builder.ResourceAddress("mesh", mesh.ResourceName)).
@@ -55,6 +58,91 @@ func TestMesh(t *testing.T) {
 		builder.AddMesh(mesh)
 
 		resource.ParallelTest(t, tfbuilder.CreatePolicyAndModifyFieldsOnIt(providerFactory, builder, mtp))
+	})
+
+	meshRetryAllSpec := `
+  spec = {
+    target_ref = {
+      kind = "Mesh"
+    }
+    to = [
+      {
+        target_ref = {
+          kind = "Mesh"
+        }
+        default = {
+          grpc = {
+            num_retries = 5
+            per_try_timeout = "16s"
+          }
+        }
+      }
+    ]
+  }
+`
+
+	// Check for fix "provider produced invalid plan for retry_on"
+	t.Run("create mesh_retry with retry_on", func(t *testing.T) {
+		builder := tfbuilder.NewBuilder(tfbuilder.KongMesh, "http", "localhost", port.Int())
+		mesh := tfbuilder.NewMeshBuilder("m3", "m3")
+		mr := tfbuilder.NewPolicyBuilder("mesh_retry", "retry_on", "retry-on", "MeshRetry").
+			WithMeshRef(builder.ResourceAddress("mesh", mesh.ResourceName) + ".name").
+			WithDependsOn(builder.ResourceAddress("mesh", mesh.ResourceName)).
+			WithSpec(meshRetryAllSpec).
+			WithLabels(map[string]string{
+				"kuma.io/mesh":   mesh.MeshName,
+				"kuma.io/env":    "universal",
+				"kuma.io/origin": "zone",
+				"kuma.io/zone":   "default",
+			})
+		builder.AddMesh(mesh)
+
+		resource.ParallelTest(t, resource.TestCase{
+			ProtoV6ProviderFactories: providerFactory,
+			Steps: []resource.TestStep{
+				{
+					Config: builder.Build(),
+				},
+				{
+					Config: builder.AddPolicy(mr).Build(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectResourceAction(builder.ResourceAddress(mr.ResourceType, mr.ResourceName), plancheck.ResourceActionCreate),
+						},
+					},
+				},
+				tfbuilder.CheckReapplyPlanEmpty(builder),
+				{
+					Config: builder.AddPolicy(mr.AddToSpec(`per_try_timeout = "16s"`, `retry_on = []`)).Build(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectResourceAction(builder.ResourceAddress(mr.ResourceType, mr.ResourceName), plancheck.ResourceActionNoop),
+						},
+					},
+				},
+				tfbuilder.CheckReapplyPlanEmpty(builder),
+				{
+					Config: builder.AddPolicy(mr.UpdateSpec(`retry_on = []`, `retry_on = ["Canceled"]`)).Build(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectResourceAction(builder.ResourceAddress(mr.ResourceType, mr.ResourceName), plancheck.ResourceActionUpdate),
+							plancheck.ExpectKnownValue(builder.ResourceAddress(mr.ResourceType, mr.ResourceName), tfjsonpath.New("spec").AtMapKey("to").AtSliceIndex(0).AtMapKey("default").AtMapKey("grpc").AtMapKey("retry_on"), knownvalue.ListExact([]knownvalue.Check{knownvalue.StringExact("Canceled")})),
+						},
+					},
+				},
+				tfbuilder.CheckReapplyPlanEmpty(builder),
+				{
+					Config: builder.AddPolicy(mr.RemoveFromSpec(`retry_on = ["Canceled"]`)).Build(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectResourceAction(builder.ResourceAddress(mr.ResourceType, mr.ResourceName), plancheck.ResourceActionUpdate),
+							plancheck.ExpectKnownValue(builder.ResourceAddress(mr.ResourceType, mr.ResourceName), tfjsonpath.New("spec").AtMapKey("to").AtSliceIndex(0).AtMapKey("default").AtMapKey("grpc").AtMapKey("retry_on"), knownvalue.Null()),
+						},
+					},
+				},
+				tfbuilder.CheckReapplyPlanEmpty(builder),
+			},
+		})
 	})
 
 	if t.Failed() {
